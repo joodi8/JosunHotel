cc4b630c181df72f64600f8a44128d13193e106a
joodi8 <8jooj8@gmail.com>
2022-02-16 17:35:35 +0900

    swiper

---
+++ js/common.js
@@ -0,0 +1,16 @@
+let swiper = new Swiper(".swiperContainer.fade1", {
+    spaceBetween: 30,
+    effect: "fade",
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
