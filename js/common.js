4581669bc1b0dbb66c19394faf3d2e2a4cf3b687
joodi8 <8jooj8@gmail.com>
2022-02-17 17:30:33 +0900

    TwT

--- js/common.js
+++ js/common.js
@@ -14,3 +14,21 @@ let swiper = new Swiper(".swiperContainer.fade1", {
         clickable: true,
     },
 });
+
+
+
+let today = new Date();   
+
+let year = today.getFullYear(); // 년도
+let month = today.getMonth() + 1;  // 월
+let date = today.getDate();  // 날짜
+let day = today.getDay();  // 요일
+let tomorrow = today.getDay() + 1;
+const week = ['일', '월', '화', '수', '목', '금', '토'];
+let dayOfWeek = week[today.getDay()];
+let tomorrowDate= year + "." + month + "." + tomorrow + dayOfWeek
+
+// document.write(year + '.' + month + '.' + date)
+// document.write('<br>')
+// document.write(day);
+document.getElementById("dateText").innerHTML = year + "." + month + "." + day + dayOfWeek + "_" + tomorrowDate;
