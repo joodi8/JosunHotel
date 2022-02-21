2ce5e3c55fef407c769685abb0dfd71957012fd2
joodi8 <8jooj8@gmail.com>
2022-02-21 16:02:49 +0900

    Mobile nev

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-// 모바일 사이즈일때 로고 이미지 변경
+// 모바일 사이즈일때 로고 이미지 변경 & nav 클릭이벤트 활성
 $(function(){
     $(window).resize(function(){
     }).resize();
@@ -11,26 +11,41 @@ $(function(){
 });
 
 
-
 $(function(){
 
     $(window).resize(function(){
     
-    var width = window.innerWidth;
+    let width = window.innerWidth;
     
     if(width < 768){
     
     $('.topLogo').attr('src', './img/header/gnb/main_logoB_column.png')
+    $('.firstWrap > li').attr('onclick','showHide(this)')
     
     }
     
-    else {$('.topLogo').attr('./img/header/gnb/main_logoB.png');}
+    else {
+        $('.topLogo').attr('./img/header/gnb/main_logoB.png');
+        $('.firstWrap > li').attr('onclick','null')
+    }
     
     }).resize();
     
     });
 
+    const showHide = (e) => {
+        const MListDrop = e.children[0];
+        const displayAttr = window.getComputedStyle(MListDrop).display;
+        // let EventAttr = window.getElementById(MListDrop).attr('onclick','showHide(this)');
+    
+        if (displayAttr == 'none') {
+            MListDrop.style.display = 'block';
+        } else {
+            MListDrop.style.display = 'none';
+        }
+    }
 
+// 스위퍼
 let headerswiper = new Swiper(".swiperContainer.fade1", {
     spaceBetween: 30,
     effect: "fade",
@@ -48,8 +63,46 @@ let headerswiper = new Swiper(".swiperContainer.fade1", {
     },
 });
 
+// nav활성화
+$ (function(){
+    let gnbFlag = false;
+    let gnbIndex;
+    
+    $(".all_menuIcon").on({
+        "click" : function() {
+            
+            if (gnbIndex != undefined) {
+                $('#all_menu').eq(gnbIndex).css('display', 'none');
+            }
+            
+            gnbIndex = $(this).index();
+            gnbFlag = true;
+            
+            $("#all_menu").removeClass("disappear")
+            $("#all_menu").addClass("appear")
+            $("#all_menu").show();
+            
+            $("#all_menu").eq(gnbIndex).css("display","block");
+        }
+    });
+    
+    $("#all_menu").on({
+        "mouseleave" : function() {
+            if(gnbFlag) {
+                $("#all_menu").removeClass("appear")
+                $("#all_menu").addClass("disappear")
+                $("#all_menu").hide();
+            } else {
+                return;
+            }
+        }
+    });
+});
 
 
+
+
+// 날짜 불러오기 (보류)
 let today = new Date();   
 
 let year = today.getFullYear(); // 년도
