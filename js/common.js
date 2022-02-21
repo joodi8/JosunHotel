b4604c776e36965f0d6a5418358115b34f42dd66
joodi8 <8jooj8@gmail.com>
2022-02-21 18:50:52 +0900

    up

--- js/common.js
+++ js/common.js
@@ -102,20 +102,16 @@ $ (function(){
 
 
 // 날짜 불러오기 (보류)
-let today = new Date();   
+// let today = new Date();   
 
-let year = today.getFullYear(); // 년도
-let month = today.getMonth() + 1;  // 월
-let date = today.getDate();  // 날짜
-let day = today.getDay();  // 요일
-let tomorrow = today.getDay() + 1;
-const week = ['일', '월', '화', '수', '목', '금', '토'];
-let dayOfWeek = week[today.getDay()];
-let plusOne = week[today.getDay() + 1];
+// let year = today.getFullYear(); 
+// let month = today.getMonth() + 1; 
+// let date = today.getDate();  
+// let day = today.getDay();  
+// let tomorrow = today.getDay() + 1;
+// const week = ['일', '월', '화', '수', '목', '금', '토'];
+// let dayOfWeek = week[today.getDay()];
+// let plusOne = week[today.getDay() + 1];
 
-let tomorrowDate= year + " . " + month + " . " + tomorrow + " (" + plusOne + ")"
-
-// document.write(year + '.' + month + '.' + date)
-// document.write('<br>')
-// document.write(day);
-document.getElementById("dateText").innerHTML = year + " . " + month + " . " + day + " (" + dayOfWeek + ")"+ " ~ " + tomorrowDate;
+// let tomorrowDate= year + " . " + month + " . " + tomorrow + " (" + plusOne + ")"
+// document.getElementById("dateText").innerHTML = year + " . " + month + " . " + day + " (" + dayOfWeek + ")"+ " ~ " + tomorrowDate;
